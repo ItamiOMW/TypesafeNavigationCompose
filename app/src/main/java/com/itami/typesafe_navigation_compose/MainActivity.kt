1993c75d39c7227e834db92064b2d4b63d92fc2e
Вадим <itamiomw@gmail.com>
2024-05-15 16:46:38 +0300

    Implementation

--- app/src/main/java/com/itami/typesafe_navigation_compose/MainActivity.kt
+++ app/src/main/java/com/itami/typesafe_navigation_compose/MainActivity.kt
@@ -3,15 +3,98 @@ package com.itami.typesafe_navigation_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.itami.typesafe_navigation_compose.ui.theme.TypesafeNavigationComposeTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TypesafeNavigationComposeTheme {
-
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = StartScreen
+                ) {
+                    composable<StartScreen> {
+                        var name by remember { mutableStateOf("") }
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "Start Screen",
+                                style = MaterialTheme.typography.titleMedium,
+                                color = MaterialTheme.colorScheme.onBackground,
+                            )
+                            Spacer(modifier = Modifier.height(12.dp))
+                            TextField(
+                                value = name,
+                                onValueChange = { newValue ->
+                                    name = newValue
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(12.dp))
+                            Button(
+                                onClick = { navController.navigate(GreetingScreen(name = name)) }
+                            ) {
+                                Text(
+                                    text = "Greet me",
+                                    style = MaterialTheme.typography.labelLarge,
+                                    color = MaterialTheme.colorScheme.onPrimary,
+                                )
+                            }
+                        }
+                    }
+                    composable<GreetingScreen> {
+                        val args = it.toRoute<GreetingScreen>()
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = "Greeting Screen.",
+                                style = MaterialTheme.typography.titleMedium,
+                                color = MaterialTheme.colorScheme.onBackground,
+                            )
+                            Spacer(modifier = Modifier.height(24.dp))
+                            Text(
+                                text = "Hello, ${args.name}! Hope you're doing well!",
+                                style = MaterialTheme.typography.labelLarge,
+                                color = MaterialTheme.colorScheme.onBackground,
+                            )
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+@Serializable
+object StartScreen
+
+@Serializable
+data class GreetingScreen(val name: String)
